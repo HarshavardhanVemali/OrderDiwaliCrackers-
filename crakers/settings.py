ce8cf53893be83dd0f0b40ce4fefbd82967ae3c6
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-10-02 18:03:52 +0530

    changescommit

--- crakers/settings.py
+++ crakers/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
